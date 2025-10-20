638fa81a137f6f6d22f4e59b95909a582c93b761
Rizky Arsyansyah Rinjani <rizky.arsyansyah@ai.astra.co.id>
2025-10-20 07:34:35 +0700

    feat: create dummy data

---
+++ data/index.ts
@@ -0,0 +1,29 @@
+export type LabelValue = { label: string; value: string };
+
+type LabelValueItem = LabelValue & { items: LabelValue[] };
+
+export const jobs: LabelValue[] = [
+  { label: "TNI", value: "tni" },
+  { label: "Polisi", value: "polisi" },
+  { label: "Guru", value: "guru" },
+  { label: "Petani", value: "petani" },
+];
+
+export const hobbies: LabelValueItem[] = [
+  {
+    label: "Membaca",
+    value: "membaca",
+    items: [
+      { label: "Novel", value: "Novel" },
+      { label: "Komik", value: "Komik" },
+    ],
+  },
+  {
+    label: "Game",
+    value: "game",
+    items: [
+      { label: "E-Sport", value: "esport" },
+      { label: "Sepak Bola", value: "sepak-bola" },
+    ],
+  },
+];
