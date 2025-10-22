91344b23177d9de9867422e0e294eb2a4dc244d9
Rizky Arsyansyah Rinjani <rizky.arsyansyah@ai.astra.co.id>
2025-10-22 16:10:15 +0700

    feat: adjust form to support onchange & onblur async

--- app/(drawer)/tanstackform.tsx
+++ app/(drawer)/tanstackform.tsx
@@ -1,77 +1,38 @@
 import { ListPicker, TextField } from "@/components/molecules";
-import { hobbies, jobs } from "@/data";
+import { hobbies, jobs, LabelValue } from "@/data";
 import { Button, Host } from "@expo/ui/swift-ui";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useForm } from "@tanstack/react-form";
-import { FC, useMemo, useState } from "react";
-import { Alert, StyleSheet, View } from "react-native";
+import { FC, useRef, useState } from "react";
+import { Alert, StyleSheet, TextInput, View } from "react-native";
 import { z } from "zod";
 
-const formSchema = z
-  .object({
-    name: z
-      .string()
-      .min(1, "Nama harus diisi")
-      .min(5, "Nama minimal 5 karakter"),
-    email: z.email("Format email salah").min(1, "Email harus diisi"),
-    job: z.object(
+const formSchema = z.object({
+  name: z.string().min(1, "Nama harus diisi").min(5, "Nama minimal 5 karakter"),
+  email: z.email("Format email salah").min(1, "Email harus diisi"),
+  job: z.object(
+    {
+      label: z.string(),
+      value: z.string(),
+    },
+    { error: "Pekerjaan harus diisi" }
+  ),
+  hobby: z
+    .object({
+      label: z.string(),
+      value: z.string(),
+    })
+    .optional(),
+  subHobby: z
+    .object(
       {
         label: z.string(),
         value: z.string(),
       },
-      { error: "Pekerjaan harus diisi" }
-    ),
-    hobby: z
-      .object(
-        {
-          label: z.string(),
-          value: z.string(),
-        }
-        // { error: "Hobi tidak valid" }
-      )
-      .optional(),
-    subHobby: z
-      .object(
-        {
-          label: z.string(),
-          value: z.string(),
-        },
-        { error: "Sub Hobi harus diisi" }
-      )
-      .optional(),
-  })
-  // .refine(
-  //   (data) => {
-  //     // ✅ If hobby is chosen, subHobby must exist
-  //     if (data.hobby) {
-  //       return !!data.subHobby;
-  //     }
-  //     return true;
-  //   },
-  //   {
-  //     message: "Sub Hobi harus diisi jika memilih Hobi",
-  //     path: ["subHobby"], // attach error to subHobby field
-  //   }
-  // );
-  // .refine(
-  //   (data) => {
-  //     if (data.hobby && !data.subHobby) return false;
-
-  //     return true;
-  //   },
-  //   {
-  //     message: "Sub Hobi harus diisi jika memilih hobi",
-  //     path: ["subHobby"],
-  //   }
-  // );
-  // .superRefine((data, ctx) => {
-  //   if (data.hobby && !data.subHobby) {
-  //     ctx.addIssue({
-  //       code: "custom",
-  //       message: "Sub Hobi harus diisi jika memilih hobi",
-  //       path: ["subHobby"],
-  //     });
-  //   }
-  // });
+      { error: "Sub Hobi harus diisi" }
+    )
+    .optional(),
+});
 
 type FormValues = z.infer<typeof formSchema>;
 
@@ -84,31 +45,36 @@ const defaultValues: FormValues = {
 };
 
 const TanstackFormScreen: FC = () => {
-  const [subHobbyLoading, setSubHobbyLoading] = useState(false);
+  const emailRef = useRef<TextInput>(null);
+
+  const [subHobbies, setSubHobbies] = useState<LabelValue[]>([]);
+  const [isSafeEmail, setIsSafeEmail] = useState(false);
 
   // @ts-ignore
   const form = useForm<FormValues>({
     defaultValues,
-    // validators: {
-    //   onSubmit: async (values) => {
-    //     // console.log("onSubmit values:", values);
-    //     const result = await formSchema.safeParseAsync(values);
-    //     console.log("result:", result);
-    //     return result.success ? undefined : result.error;
-    //   },
-    // },
     onSubmit: async ({ value }) => {
+      const { hobby, subHobby } = value;
+
+      if (hobby && !subHobby) return form.setFieldValue("subHobby", null);
+
       Alert.alert("Values", JSON.stringify(value));
     },
   });
 
-  const { state, Field } = form;
+  const { Field, Subscribe } = form;
 
-  // Derived subHobby options
-  const subHobbies = useMemo(() => {
-    const hobby = state.values.hobby;
-    return hobbies.find((h) => h.value === hobby?.value)?.items || [];
-  }, [state.values.hobby]);
+  const onHobbyChange = async ({ fieldApi, value }) => {
+    const formApi = fieldApi.form;
+    if (formApi.state.isSubmitting) return;
+
+    await new Promise((r) => setTimeout(r, 1000));
+
+    const matched = hobbies.find((h) => h.value === value?.value);
+    setSubHobbies(matched?.items ?? []);
+
+    formApi.setFieldValue("subHobby", undefined);
+  };
 
   return (
     <View style={styles.root}>
@@ -128,29 +94,65 @@ const TanstackFormScreen: FC = () => {
             onChange={handleChange}
             status={errors.length ? "error" : undefined}
             helper={errors[0]?.message}
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current?.focus()}
           />
         )}
       </Field>
 
       {/* Email */}
-      <Field name="email" validators={{ onChange: formSchema.shape.email }}>
+      <Field
+        name="email"
+        validators={{
+          onChange: formSchema.shape.email,
+          onBlurAsync: async ({ value, fieldApi }) => {
+            const formApi = fieldApi.form;
+            if (!value || formApi.state.isSubmitting) return;
+
+            await new Promise((r) => setTimeout(r, 1500));
+
+            const isDuplicated = value.toLowerCase() === "andi@rubin.com";
+
+            if (!isSafeEmail) setIsSafeEmail(!isDuplicated);
+
+            if (isDuplicated) {
+              throw new Error("Email sudah terdaftar");
+            }
+          },
+        }}
+      >
         {({
           state: {
             value,
-            meta: { errors },
+            meta: { errors, isValidating, isValid },
           },
           handleChange,
+          handleBlur,
         }) => (
           <TextField
+            ref={emailRef}
             label="Email"
             placeholder="Masukan Email"
             keyboardType="email-address"
             inputMode="email"
             autoCapitalize="none"
             value={value}
-            onChange={handleChange}
+            onChange={(value) => {
+              if (isSafeEmail) setIsSafeEmail(false);
+              handleChange(value);
+            }}
+            onBlur={handleBlur}
             status={errors.length ? "error" : undefined}
             helper={errors[0]?.message}
+            loading={isValidating}
+            returnKeyType="next"
+            EndComponent={
+              isValid &&
+              !isValidating &&
+              isSafeEmail && (
+                <MaterialIcons name="verified" color="green" size={20} />
+              )
+            }
           />
         )}
       </Field>
@@ -177,7 +179,14 @@ const TanstackFormScreen: FC = () => {
       </Field>
 
       {/* Hobby */}
-      <Field name="hobby" validators={{ onChange: formSchema.shape.hobby }}>
+      <Field
+        name="hobby"
+        validators={{
+          onChange: formSchema.shape.hobby,
+          onChangeAsyncDebounceMs: 100,
+          onChangeAsync: onHobbyChange,
+        }}
+      >
         {({
           state: {
             value,
@@ -190,12 +199,7 @@ const TanstackFormScreen: FC = () => {
             placeholder="Pilih Hobi"
             data={hobbies}
             value={value}
-            onChange={(v) => {
-              setSubHobbyLoading(true);
-              handleChange(v);
-              form.setFieldValue("subHobby", null);
-              setTimeout(() => setSubHobbyLoading(false), 1000);
-            }}
+            onChange={handleChange}
             status={errors.length ? "error" : undefined}
             helper={errors[0]?.message}
           />
@@ -203,33 +207,43 @@ const TanstackFormScreen: FC = () => {
       </Field>
 
       {/* Sub Hobby */}
-      <Field
-        name="subHobby"
-        validators={{ onChange: formSchema.shape.subHobby }}
+      <Subscribe
+        selector={(state) => [
+          state.fieldMeta.hobby?.isValidating,
+          state.values.hobby,
+          state.isSubmitting,
+        ]}
       >
-        {({
-          state: {
-            value,
-            meta: { errors },
-          },
-          handleChange,
-        }) => (
-          <ListPicker
-            label="Sub Hobi"
-            placeholder="Pilih Sub Hobi"
-            data={subHobbies}
-            value={value}
-            onChange={handleChange}
-            disabled={!form.state.values.hobby}
-            loading={subHobbyLoading}
-            status={errors.length ? "error" : undefined}
-            helper={errors[0]?.message}
-          />
+        {([isValidating, hobby, isSubmitting]) => (
+          <Field
+            name="subHobby"
+            validators={{ onChange: formSchema.shape.subHobby }}
+          >
+            {({
+              state: {
+                value,
+                meta: { errors },
+              },
+              handleChange,
+            }) => (
+              <ListPicker
+                label="Sub Hobi"
+                placeholder="Pilih Sub Hobi"
+                data={subHobbies}
+                value={value}
+                onChange={handleChange}
+                loading={isValidating && !isSubmitting}
+                status={errors.length ? "error" : undefined}
+                helper={errors[0]?.message}
+                disabled={!hobby}
+              />
+            )}
+          </Field>
         )}
-      </Field>
+      </Subscribe>
 
       <Host matchContents>
-        <Button variant="borderedProminent" onPress={() => form.handleSubmit()}>
+        <Button variant="borderedProminent" onPress={form.handleSubmit}>
           Submit
         </Button>
       </Host>
