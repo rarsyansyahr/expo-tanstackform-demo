37bfdb5d44ae37ae6e3dd7c5b3d5d60b6aaa1d9f
Rizky Arsyansyah Rinjani <rizky.arsyansyah@ai.astra.co.id>
2025-10-28 14:59:29 +0700

    feat: create array field for education histories

--- app/(drawer)/tanstackform.tsx
+++ app/(drawer)/tanstackform.tsx
@@ -3,56 +3,97 @@ import {
   DatePickerRef,
   ListPicker,
   ListPickerRef,
+  RadioGroup,
   TextField,
 } from "@/components/molecules";
 import { hobbies, jobs, LabelValue } from "@/data";
-import { Button, Host } from "@expo/ui/swift-ui";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useForm } from "@tanstack/react-form";
 import { FC, useRef, useState } from "react";
-import { Alert, Keyboard, StyleSheet, TextInput, View } from "react-native";
+import {
+  Alert,
+  Button,
+  FlatList,
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { z } from "zod";
 
-const formSchema = z.object({
-  name: z.string().min(1, "Nama harus diisi").min(5, "Nama minimal 5 karakter"),
-  birthDate: z.object(
-    {
-      label: z.string(),
-      value: z.date().max(new Date(), "Tanggal Lahir harus di bawah hari ini"),
-    },
-    { error: "Tanggal Lahir harus diisi" }
-  ),
-  email: z.email("Format email salah").min(1, "Email harus diisi"),
-  job: z.object(
-    {
-      label: z.string(),
-      value: z.string(),
-    },
-    { error: "Pekerjaan harus diisi" }
-  ),
-  hobby: z
-    .object({
-      label: z.string(),
-      value: z.string(),
-    })
-    .optional(),
-  subHobby: z
-    .object(
+const educationItemSchema = z.object({
+  school: z.string().min(5, "Instansi Pendidikan harus diisi"),
+  degree: z.string().min(5, "Gelar harus diisi"),
+  yearRange: z.string().min(5, "Tahun Pendidikan harus diisi"),
+});
+
+const formSchema = z
+  .object({
+    name: z
+      .string()
+      .min(1, "Nama harus diisi")
+      .min(5, "Nama minimal 5 karakter"),
+    birthDate: z.object(
+      {
+        label: z.string(),
+        value: z
+          .date()
+          .max(new Date(), "Tanggal Lahir harus di bawah hari ini"),
+      },
+      { error: "Tanggal Lahir harus diisi" }
+    ),
+    email: z.email("Format email salah").min(1, "Email harus diisi"),
+    gender: z.enum(["male", "female"], "Jenis Kelamin harus diisi"),
+    educations: z
+      .array(educationItemSchema)
+      .min(1, "Riwayat Pendidikan harus diisi"),
+    job: z.object(
       {
         label: z.string(),
         value: z.string(),
       },
-      { error: "Sub Hobi harus diisi" }
-    )
-    .optional(),
-});
+      { error: "Pekerjaan harus diisi" }
+    ),
+    hobby: z
+      .object({
+        label: z.string(),
+        value: z.string(),
+      })
+      .optional(),
+    subHobby: z
+      .object(
+        {
+          label: z.string(),
+          value: z.string(),
+        },
+        { error: "Sub Hobi harus diisi" }
+      )
+      .optional(),
+  })
+  // .refine((values) => {
+  //   if (values.hobby && !values.subHobby) {
+  //     val
+  //   }
+  // });
 
 type FormValues = z.infer<typeof formSchema>;
 
 const defaultValues: FormValues = {
   name: "",
   email: "andi@rubin.com",
+  gender: undefined as any,
   birthDate: undefined as any,
+  educations: [
+    {
+      school: "",
+      degree: "",
+      yearRange: "",
+    },
+  ],
   job: jobs[0],
   hobby: undefined as any,
   subHobby: undefined as any,
@@ -67,6 +108,8 @@ const TanstackFormScreen: FC = () => {
 
   const [subHobbies, setSubHobbies] = useState<LabelValue[]>([]);
   const [isSafeEmail, setIsSafeEmail] = useState(false);
+
+  const insets = useSafeAreaInsets();
 
   // @ts-ignore
   const form = useForm<FormValues>({
@@ -98,7 +141,10 @@ const TanstackFormScreen: FC = () => {
   };
 
   return (
-    <View style={styles.root}>
+    <ScrollView
+      contentContainerStyle={[styles.root, { paddingBottom: insets.bottom }]}
+      showsVerticalScrollIndicator={false}
+    >
       {/* Name */}
       <Field name="name" validators={{ onChange: formSchema.shape.name }}>
         {({
@@ -179,6 +225,29 @@ const TanstackFormScreen: FC = () => {
         )}
       </Field>
 
+      {/* Gender */}
+      <Field name="gender" validators={{ onChange: formSchema.shape.gender }}>
+        {({
+          state: {
+            value,
+            meta: { errors },
+          },
+          handleChange,
+        }) => (
+          <RadioGroup
+            label="Jenis Kelamin"
+            data={[
+              { label: "Laki-laki", value: "male" },
+              { label: "Perempuan", value: "female" },
+            ]}
+            onChange={handleChange}
+            value={value}
+            status={errors.length ? "error" : undefined}
+            helper={errors[0]?.message}
+          />
+        )}
+      </Field>
+
       {/* Birth Date */}
       <Field
         name="birthDate"
@@ -208,6 +277,147 @@ const TanstackFormScreen: FC = () => {
             }}
             status={errors.length ? "error" : undefined}
             helper={errors[0]?.message}
+          />
+        )}
+      </Field>
+
+      {/* Educations */}
+      <Field
+        name="educations"
+        mode="array"
+        validators={{ onChange: formSchema.shape.educations }}
+      >
+        {(field) => (
+          <FlatList
+            data={field.state.value}
+            extraData={field.state.value}
+            renderItem={({ index, item }) => (
+              <View
+                key={index}
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 6,
+                  flex: 1,
+                }}
+              >
+                <View style={{ flex: 1, gap: 6 }}>
+                  <Field
+                    name={`educations[${index}].school`}
+                    key={`school-${index}`}
+                    validators={{ onChange: educationItemSchema.shape.school }}
+                  >
+                    {({
+                      handleChange,
+                      handleBlur,
+                      state: {
+                        value,
+                        meta: { errors },
+                      },
+                    }) => (
+                      <TextField
+                        label="Instansi Pendidikan"
+                        placeholder="Masukan Instansi Pendidikan"
+                        onChange={handleChange}
+                        value={value}
+                        onBlur={handleBlur}
+                        status={errors.length ? "error" : undefined}
+                        helper={errors[0]?.message}
+                      />
+                    )}
+                  </Field>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      // alignItems: "center",
+                      gap: 6,
+                    }}
+                  >
+                    <Field
+                      name={`educations[${index}].degree`}
+                      key={`degree-${index}`}
+                      validators={{
+                        onChange: educationItemSchema.shape.degree,
+                      }}
+                    >
+                      {({
+                        handleChange,
+                        handleBlur,
+                        state: {
+                          value,
+                          meta: { errors },
+                        },
+                      }) => (
+                        <TextField
+                          label="Program Studi"
+                          placeholder="Masukan Program Studi"
+                          onChange={handleChange}
+                          value={value}
+                          style={{ flex: 1 }}
+                          onBlur={handleBlur}
+                          status={errors.length ? "error" : undefined}
+                          helper={errors[0]?.message}
+                        />
+                      )}
+                    </Field>
+                    <Field
+                      name={`educations[${index}].yearRange`}
+                      key={`yearRange-${index}`}
+                      validators={{
+                        onChange: educationItemSchema.shape.yearRange,
+                      }}
+                    >
+                      {({
+                        handleChange,
+                        handleBlur,
+                        state: {
+                          value,
+                          meta: { errors },
+                        },
+                      }) => (
+                        <TextField
+                          label="Tahun Pendidikan"
+                          placeholder="Masukan Tahun Pendidikan"
+                          onChange={handleChange}
+                          value={value}
+                          style={{ width: "40%" }}
+                          onBlur={handleBlur}
+                          status={errors.length ? "error" : undefined}
+                          helper={errors[0]?.message}
+                        />
+                      )}
+                    </Field>
+                  </View>
+                </View>
+                <TouchableOpacity
+                  style={{ alignItems: "center", justifyContent: "center" }}
+                  activeOpacity={0.8}
+                  onPress={() =>
+                    field.state.value.length > 1 && field.removeValue(index)
+                  }
+                >
+                  <MaterialIcons name="close" size={20} />
+                </TouchableOpacity>
+              </View>
+            )}
+            contentContainerStyle={{ gap: 6 }}
+            removeClippedSubviews
+            showsVerticalScrollIndicator={false}
+            scrollEnabled={false}
+            ItemSeparatorComponent={() => (
+              <View
+                style={{ borderWidth: 0.3, borderColor: "black", marginTop: 6 }}
+              />
+            )}
+            ListHeaderComponent={<Text>Riwayat Pendidikan</Text>}
+            ListFooterComponent={
+              <Button
+                onPress={() =>
+                  field.pushValue({ school: "", degree: "", yearRange: "" })
+                }
+                title="Tambah Pendidikan"
+              />
+            }
           />
         )}
       </Field>
@@ -303,12 +513,8 @@ const TanstackFormScreen: FC = () => {
         )}
       </Subscribe>
 
-      <Host matchContents>
-        <Button variant="borderedProminent" onPress={form.handleSubmit}>
-          Submit
-        </Button>
-      </Host>
-    </View>
+      <Button onPress={form.handleSubmit} title="Submit" />
+    </ScrollView>
   );
 };
 
