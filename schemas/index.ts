15f8f5ac16224389a3d574c9d8fd5e7ab415a26b
Rizky Arsyansyah Rinjani <rizky.arsyansyah@ai.astra.co.id>
2025-10-30 16:29:34 +0700

    feat: update schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,9 +1,9 @@
 import z from "zod";
 
 export const educationItemSchema = z.object({
-  school: z.string().min(5, "Instansi Pendidikan harus diisi"),
-  degree: z.string().min(5, "Gelar harus diisi"),
-  yearRange: z.string().min(5, "Tahun Pendidikan harus diisi"),
+  school: z.string("Instansi Pendidikan harus diisi"),
+  degree: z.string("Gelar harus diisi"),
+  yearRange: z.string("Tahun Pendidikan harus diisi"),
 });
 
 const labelValueItem = {
@@ -11,19 +11,41 @@ const labelValueItem = {
   value: z.string(),
 };
 
-export const tanstackFormSchema = z.object({
+export const personalFormSchema = z.object({
   name: z.string("Nama harus diisi").min(5, "Nama minimal 5 karakter"),
   birthDate: z
     .date("Tanggal lahir harus diisi")
     .max(new Date(), "Tanggal Lahir harus di bawah hari ini"),
-  email: z.email("Format email salah").min(1, "Email harus diisi"),
+  email: z.string("Email harus diisi").email("Format email salah"),
   gender: z.enum(["male", "female"], "Jenis Kelamin harus diisi"),
-  educations: z.array(educationItemSchema, "Riwayat Pendidikan harus diisi"),
+});
+
+export type PersonalFormValues = z.infer<typeof personalFormSchema>;
+
+const jobFormSchema = z.object({
   job: z.object(labelValueItem, { error: "Pekerjaan harus diisi" }),
+});
+
+export type JobFormValues = z.infer<typeof jobFormSchema>;
+
+const otherFormSchema = z.object({
   hobby: z.object(labelValueItem).optional(),
   subHobby: z
     .object(labelValueItem, { error: "Sub Hobi harus diisi" })
     .optional(),
 });
+
+export type OtherFormValues = z.infer<typeof otherFormSchema>;
+
+const educationFormSchema = z.object({
+  educations: z.array(educationItemSchema, "Riwayat Pendidikan harus diisi"),
+});
+
+export type EducationFormValues = z.infer<typeof educationFormSchema>;
+
+export const tanstackFormSchema = personalFormSchema
+  .merge(jobFormSchema)
+  .merge(otherFormSchema)
+  .merge(educationFormSchema);
 
 export type TanstackFormValues = z.infer<typeof tanstackFormSchema>;
